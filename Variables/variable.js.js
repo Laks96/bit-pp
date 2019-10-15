7a678d5ae83fb1e4481cc14759e02f55d367005b
BIT Student <classroom@bgit.rs>
2019-10-15 14:59:21 +0200

    first test

---
+++ Variables/variable.js.js
@@ -0,0 +1,9 @@
+var m = 100;
+var p = 99;
+
+if (m > p) {
+    console.log("buy it, the rest of money " + (m - p) + "$");
+}
+else {
+    console.log(" you cannot buy it because you need " + (p - m) + "$  more");
+}
